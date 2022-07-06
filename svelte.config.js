b9c90f7d6aa65357018c91b09c2efebe9f6b7326
Zeyad Alagamy <wezze2003@gmail.com>
2022-07-06 22:03:15 +0200

    Fix resources

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,7 @@ import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
+const dev = "production" === "development";
 const config = {
 	preprocess: preprocess(),
 
@@ -10,7 +11,13 @@ const config = {
 			defalult: false,
             fallback: 'index.html'
 		}),
-
+        paths: {
+            // change below to your repo name
+            base: dev ? '' : '/zeyadajamy.github.io',
+        },
+		files: {
+			assets: 'src/lib/assets',
+		},
 		// Override http methods in the Todo forms
 		methodOverride: {
 			allowed: ['PATCH', 'DELETE']
