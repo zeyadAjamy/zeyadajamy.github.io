57dda6edd6b7e292caf89afb131d62c3ae6c1259
Zeyad Alagamy <wezze2003@gmail.com>
2022-07-07 01:05:28 +0200

    Fix resources

--- svelte.config.js
+++ svelte.config.js
@@ -18,7 +18,6 @@ const config = {
 			lib: 'src/lib',
 			routes: 'src/routes'
 		},
-		appDir: 'internal',
 		// Override http methods in the Todo forms
 		methodOverride: {
 			allowed: ['PATCH', 'DELETE']
