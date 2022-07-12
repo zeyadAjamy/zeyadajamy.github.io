805ca4fa5803156894d8752f68c484da34255345
Zeyad Alagamy <wezze2003@gmail.com>
2022-07-12 19:38:15 +0200

    Fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,24 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
-}
+	env: {
+		browser: true,
+		es2021: true
+	},
+	extends: ['Airbnb', 'plugin:@typescript-eslint/recommended', 'prettier'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module'
+	},
+	plugins: ['svelte3', '@typescript-eslint'],
+	settings: {
+		'import/resolver': {
+			typescript: {} // this loads <rootdir>/tsconfig.json to eslint
+		}
+	},
+	rules: {
+		'no-tabs': 0,
+		indent: [2, 'tab'],
+		'@typescript-eslint/no-var-requires': 0,
+		quotes: [2, 'single']
+	}
+};
